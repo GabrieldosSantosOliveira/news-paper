c79f813e93bb131b48869fba2407a7d6cf159e3b
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-24 15:04:57 -0400

    add path alias

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,24 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@/components': './src/components',
+            '@/contexts': './src/contexts',
+            '@/errors': './src/errors',
+            '@/hooks': './src/hooks',
+            '@/interfaces': './src/interfaces',
+            '@/routes': './src/routes',
+            '@/screens': './src/screens',
+            '@/services': './src/services',
+            '@/styles': './src/styles',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
