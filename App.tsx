a0e33da2a181a292d00039e49f89eac0d9c760d8
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-24 16:26:59 -0400

    loaded fonts and add providers

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Loading, StatusBar } from '@/components';
+import {
+  HttpServiceProvider,
+  StorageProvider,
+  ThemeProvider,
+} from '@/contexts';
+import { Home } from '@/screens/Home/Home';
+import {
+  useFonts,
+  Lexend_400Regular,
+  Lexend_500Medium,
+  Lexend_600SemiBold,
+  Lexend_700Bold,
+} from '@expo-google-fonts/lexend';
+import { Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { SafeAreaView } from 'react-native-safe-area-context';
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    Lexend_400Regular,
+    Lexend_500Medium,
+    Lexend_600SemiBold,
+    Lexend_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Poppins_600SemiBold,
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HttpServiceProvider>
+      <StorageProvider>
+        <ThemeProvider>
+          <StatusBar />
+          <SafeAreaView style={{ flex: 1 }}>
+            {isFontsLoaded ? <Home /> : <Loading />}
+          </SafeAreaView>
+        </ThemeProvider>
+      </StorageProvider>
+    </HttpServiceProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
