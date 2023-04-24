2a2cca2ce491b10a19055eeb26aa1e56cdfc4ec8
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-24 15:37:18 -0400

    renamed interfaces

---
+++ src/interfaces/HttpService.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface HttpServiceResponse<T = any> {
+  statusCode: number;
+  data: T;
+}
+
+export interface HttpServiceInterceptorsResponse {
+  onRejected(response: HttpServiceResponse): Promise<HttpServiceResponse>;
+}
+export interface HttpServiceInterceptors {
+  response: HttpServiceInterceptorsResponse;
+}
+export interface HttpServiceOptionsHeaders {
+  contentType?: string;
+}
+export interface HttpServiceOptions {
+  body?: any;
+  headers?: HttpServiceOptionsHeaders;
+}
+export abstract class HttpService {
+  public abstract interceptors: HttpServiceInterceptors | null;
+  abstract get<T = any>(
+    url: string,
+    options?: HttpServiceOptions,
+  ): Promise<HttpServiceResponse<T>>;
+  abstract post<T = any>(
+    url: string,
+    options?: HttpServiceOptions,
+  ): Promise<HttpServiceResponse<T>>;
+  abstract put<T = any>(
+    url: string,
+    options?: HttpServiceOptions,
+  ): Promise<HttpServiceResponse<T>>;
+  abstract delete<T = any>(
+    url: string,
+    options?: HttpServiceOptions,
+  ): Promise<HttpServiceResponse<T>>;
+  abstract patch<T = any>(
+    url: string,
+    options?: HttpServiceOptions,
+  ): Promise<HttpServiceResponse<T>>;
+}
--- src/interfaces/Storage.ts
+++ src/interfaces/Storage.ts
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export type IStorageMultiGet = any | null[];
-export interface IStorage {
+export interface Storage {
   get<T = any>(key: string): Promise<T | null>;
   set(key: string, value: any): Promise<void>;
   remove(key: string): Promise<void>;
   multiRemove(keys: string[]): Promise<void>;
   multiSet(keysAndValues: [string, any][]): Promise<void>;
-  multiGet(keys: string[]): Promise<IStorageMultiGet>;
+  multiGet(keys: string[]): Promise<any | null[]>;
 }
