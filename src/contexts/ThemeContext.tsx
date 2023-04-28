41ff4e6b59d6f4e709374df11217de8604e4e085
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-28 14:34:54 -0400

    fix: removed theme gray

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -2,8 +2,8 @@ import { ThemeLight } from '@/styles/Theme';
 import { createContext, FC, ReactNode } from 'react';
 export interface IThemeContext {
   theme: typeof ThemeLight;
-  currentTheme: 'light' | 'gray' | 'dark' | 'automatic';
-  themeInUse: 'light' | 'gray' | 'dark';
+  currentTheme: 'light' | 'dark' | 'automatic';
+  colorMode: 'light' | 'dark';
 }
 export const ThemeContext = createContext<IThemeContext>({} as IThemeContext);
 export interface IThemeProvider {
@@ -13,7 +13,7 @@ export interface IThemeProvider {
 export const ThemeProvider: FC<IThemeProvider> = ({ children }) => {
   return (
     <ThemeContext.Provider
-      value={{ currentTheme: 'light', theme: ThemeLight, themeInUse: 'light' }}
+      value={{ currentTheme: 'light', theme: ThemeLight, colorMode: 'light' }}
     >
       {children}
     </ThemeContext.Provider>
