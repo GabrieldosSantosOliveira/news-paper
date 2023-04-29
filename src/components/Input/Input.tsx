87972556eb3f592cf8e6b6e51d7726377b6bcaa1
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-28 23:59:04 -0400

    feat: add component Input Input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,29 @@
+import { useInput, useTheme } from '@/hooks';
+import { FC, memo } from 'react';
+import { TextInput, TextInputProps } from 'react-native';
+export type InputProps = TextInputProps;
+const InputBase: FC<InputProps> = ({ onBlur, onFocus, style, ...props }) => {
+  const { changeToWithoutFocus, changeToWithFocus } = useInput();
+  const {
+    theme: { fonts, colors },
+  } = useTheme();
+  return (
+    <TextInput
+      style={[
+        { flex: 1, fontFamily: fonts.Lexend[500], color: colors.text.primary },
+        style,
+      ]}
+      placeholderTextColor={colors.text.subTitle}
+      {...props}
+      onBlur={(e) => {
+        changeToWithoutFocus();
+        onBlur ? onBlur(e) : null;
+      }}
+      onFocus={(e) => {
+        changeToWithFocus();
+        onFocus ? onFocus(e) : null;
+      }}
+    />
+  );
+};
+export const Input = memo(InputBase);
