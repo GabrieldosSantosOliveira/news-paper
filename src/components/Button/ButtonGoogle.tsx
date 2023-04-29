10bf2282eaee62292d61074a92f4b8f34cac4fa1
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-28 23:54:45 -0400

    feat: add component ButtonGoogle

---
+++ src/components/Button/ButtonGoogle.tsx
@@ -0,0 +1,72 @@
+import GoogleLogo from '@/assets/images/google-icon.svg';
+import { useTheme } from '@/hooks';
+import { FC } from 'react';
+import {
+  ActivityIndicator,
+  GestureResponderEvent,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import { BoxEmpty } from '../BoxEmpty';
+export interface ButtonGoogleProps {
+  onPress: (event: GestureResponderEvent) => void;
+  isLoading?: boolean;
+}
+export const ButtonGoogle: FC<ButtonGoogleProps> = ({
+  onPress,
+  isLoading = false,
+}) => {
+  const {
+    theme: { size, colors, fontSize, fonts },
+  } = useTheme();
+  return (
+    <TouchableOpacity
+      disabled={isLoading}
+      style={{
+        height: size[20],
+        backgroundColor: colors.button.google,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        paddingHorizontal: 6,
+      }}
+      onPress={onPress}
+    >
+      {isLoading ? (
+        <ActivityIndicator size="small" color={colors.text.google} />
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: colors.text.google,
+              borderRadius: 8,
+              padding: 8,
+            }}
+          >
+            <GoogleLogo height={24} width={24} />
+          </View>
+          <Text
+            style={{
+              fontFamily: fonts.Roboto[500],
+              fontSize: fontSize[15],
+              color: colors.text.google,
+            }}
+          >
+            Continuar com o Google
+          </Text>
+          <BoxEmpty />
+        </View>
+      )}
+    </TouchableOpacity>
+  );
+};
