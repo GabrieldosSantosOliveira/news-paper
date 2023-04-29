b1ce1a543f91c717824470115476c80eee55133b
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-29 00:05:43 -0400

    feat: add component Header to screen SingUp

---
+++ src/screens/SingUp/components/Header.tsx
@@ -0,0 +1,15 @@
+import { useTheme } from '@/hooks';
+import { AntDesign } from '@expo/vector-icons';
+import { TouchableOpacity, View } from 'react-native';
+export const Header = () => {
+  const {
+    theme: { size, colors },
+  } = useTheme();
+  return (
+    <View style={{ width: '100%', height: size[20], justifyContent: 'center' }}>
+      <TouchableOpacity>
+        <AntDesign name="arrowleft" size={24} color={colors.text.primary} />
+      </TouchableOpacity>
+    </View>
+  );
+};
